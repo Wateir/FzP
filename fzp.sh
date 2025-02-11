996d8dbe95e5e94cb0f0091dc83f75fcc1c4df7b
Wateir <comptesbidon.fr@gmail.com>
2025-02-11 14:53:04 +0100

    Start of help menu

--- fzp.sh
+++ fzp.sh
@@ -3,7 +3,22 @@
 FZF_OPTIONS="--info=right --reverse --min-height=5"
 arguments=("install" "remove" "package" "list")
 
+function help(){
+	echo "FzP a fuzzy package manager"
+	echo "	Usage : $0 [-h|s] [Argument]"
+	echo "OPTIONS"
+	echo "	-h 		this menu"
+	echo "	-s 		open it on a small window and not full screen"
+	echo "ARGUMENT"
+	echo "	list		Give choice between sort of package currently install"
+	echo "	package		Option to manage a specified package"
+	
+}
 
+if [ "$1" = "--help" ];then
+	help
+	exit 0
+fi
 
 while getopts "hs:" opt; do
 	case "$opt" in
@@ -21,17 +36,11 @@ while getopts "hs:" opt; do
 	esac
 done
 
-
 if ! echo "${arguments[@]}" | grep -qw "$1"; then
     echo "$0 : '$1' is not a $O command. See '$0 --help'"
     exit 1
 fi
 
-function help(){
-	echo "Usage : $0 [-h] [-s]"
-	echo "-h 	this menu"
-	echo "-s 	open it on a small window and not full screen"
-}
 			
 if [ "$1" = "list" ]; then
 	source ./src/pacQ.sh "$FZF_OPTIONS"
