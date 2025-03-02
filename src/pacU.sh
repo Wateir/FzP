36d455e7378b6daf1546c885111544cc1c020366
Wateir <comptesbidon.fr@gmail.com>
2025-03-02 02:43:35 +0100

    Add some updates check and info
    add check before execute command if updates is need, add some useful info (or not) on the fzf preview

--- src/pacU.sh
+++ src/pacU.sh
@@ -1,10 +1,16 @@
 PACU_OPTIONS=("Yes" "No" "Custom")
 
+if [ $(checkupdates | wc -l) -eq 0 ]; then
+	echo "Nothing need update"
+	exit 0
+fi
 
 selected=$(printf "%s\n" "${PACU_OPTIONS[@]}" | fzf \
 	$1 \
-	--preview "checkupdates | bat
-			   "\
+	--preview "echo 'FzP update utility'
+	echo '	- Update all what need to be update'
+	checkupdates | bat -fl yml --style grid,numbers --terminal-width \$FZF_PREVIEW_COLUMNS
+	"\
 	--preview-window 'right:70%:wrap:noinfo' \
 	--no-input)
 
@@ -12,10 +18,14 @@ if [ "$selected" = "Yes" ]; then
 
 	sudo pacman -Syu
 	paru -Syu
+	echo ""
 	echo "Don't forget to update zinit"
+	echo ""
+	echo "Done - Press enter to exit"; read
 
 		
 elif [ "$selected" = "No" ]; then
+	echo "End by user"
 	exit 0
 elif [ "$selected" = "Custom" ]; then
 	echo "Custom"
